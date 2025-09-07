94cf9c2ac6be6558669e832cb4d3cba952ad411a
okada_env <k.okada@env-value.co.jp>
2025-09-08 08:48:52 +0900

    レイアウトの調整

--- client/src/components/TaskListComponent.vue
+++ client/src/components/TaskListComponent.vue
@@ -33,16 +33,18 @@ const showTask = (index) => {
 </script>
 
 <template>
-   <div class="container">
-         <ul> 
+   <div class="container d-flex justify-content-center">
+         <div class="text-center">
             <h2>タスク一覧</h2>
-            <li v-for="(task, i) in tasks" v-bind:key="i">
-                タスク名：{{ task.title }}
-                <button v-on:click="showTask(i)">詳細</button>
-                <button v-on:click="editTask(i)">編集</button>
-                <button v-on:click="deleteTask(task.id)">Delete</button>
-            </li>
-         </ul>
+            <ul class="list-unstyled"> 
+                <li v-for="(task, i) in tasks" v-bind:key="i" class="mb-2">
+                    タスク名：{{ task.title }}
+                    <button v-on:click="showTask(i)" class="btn btn-sm btn-primary ms-2">詳細</button>
+                    <button v-on:click="editTask(i)" class="btn btn-sm btn-warning ms-2">編集</button>
+                    <button v-on:click="deleteTask(task.id)" class="btn btn-sm btn-danger ms-2">Delete</button>
+                </li>
+            </ul>
+         </div>
    </div>
 </template>
    
